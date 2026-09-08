fa4f1290a2740816eb997d6d3f22117548f29d21
Jimmy Murillo <salchichongallo1997@gmail.com>
2026-09-08 03:56:47 -0500

    Initial Commit

---
+++ rotatingquotes/rotatingquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotatingquotesApp.swift
+//  rotatingquotes
+//
+//  Created by Jaime Gallo on 8/09/26.
+//
+
+import SwiftUI
+
+@main
+struct rotatingquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ widget/widgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Jaime Gallo on 8/09/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+    }
+}
